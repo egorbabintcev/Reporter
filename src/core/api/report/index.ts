5cfe43ae4cfcb32a840faeea6869b758e254043d
Егор Бабинцев <e.e.babintsev@yandex.ru>
2022-09-03 11:43:31 +0300

    Добавлен api сервис отчетов
    - Добавлен api сервис для работы с одним и несколькими отчетами
    - Добавлен порт, описывающий интерфейс api сервиса

---
+++ src/core/api/report/index.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { RawReport, Report } from '@/core/domain/report';
+import { OmittedReport, ReportApiPort } from '@/core/ports/report/apiPort';
+import useReportAdapter from '@/core/adapters/report';
+
+export default function useReportApi(): ReportApiPort {
+  const reportAdapter = useReportAdapter();
+
+  async function readReport(id: Report['id']) {
+    const response = await axios.get<{ report: RawReport }>(`/api/v1/reports/${id}`);
+    const { report } = response.data;
+
+    return reportAdapter.convertReportFromAPI(report);
+  }
+
+  async function readReports() {
+    const response = await axios.get<{ reports: RawReport[] }>('/api/v1/reports');
+    const { reports } = response.data;
+
+    return reportAdapter.convertReportListFromAPI(reports);
+  }
+
+  async function createReport(report: OmittedReport) {
+    const response = await axios.post<Pick<Report, 'id'>>('api/v1/reports', report);
+
+    return response.data;
+  }
+
+  async function updateReport(id: Report['id'], report: OmittedReport) {
+    await axios.patch<void>(`/api/v1/reports/${id}`, report);
+  }
+
+  async function deleteReport(id: Report['id']) {
+    await axios.delete<void>(`/api/v1/reports/${id}`);
+  }
+
+  return {
+    readReport,
+    readReports,
+    createReport,
+    updateReport,
+    deleteReport,
+  };
+}
---
+++ src/core/ports/report/apiPort.ts
@@ -0,0 +1,11 @@
+import { Report } from '@/core/domain/report';
+
+export type OmittedReport = Omit<Report, 'id' | 'createdAt' | 'creatorId'>
+
+export type ReportApiPort = {
+  readReport(id: Report['id']): Promise<Report | null>
+  readReports(): Promise<Report[]>
+  createReport(report: OmittedReport): Promise<Pick<Report, 'id'>>
+  updateReport(id: Report['id'], report: OmittedReport): Promise<void>
+  deleteReport(id: Report['id']): Promise<void>
+}
