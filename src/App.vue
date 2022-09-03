3d572ff219bbf54ce8f9a4be79d68c1f3aabf36a
Егор Бабинцев <e.e.babintsev@yandex.ru>
2022-09-03 12:01:14 +0300

    Добавлена поддержка sass
    - Добавлена базовая директория стилей
    - Добавлены пакеты для поддержки sass на уровне сборщика

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<template>
-  <h1>Hello, world!</h1>
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 
@@ -10,13 +6,6 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
