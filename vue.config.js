7c80535246843f10da41e17162452a1e1661d880
Егор Бабинцев <babintsev@trueconf.ru>
2022-09-03 11:21:49 +0300

    Добавлены базовые библиотеки
    - Добавлены базовые утилитарные библиотеки
    - Конфигурация сборщика расширена использованием env переменной
    - Отредактирован README
    - Удалены ненужные файлы, оставшиеся от инициализации

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
 const { defineConfig } = require('@vue/cli-service');
 
+const {
+  proxy = 'http://localhost:8888',
+} = process.env;
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: proxy,
+        changeOrigin: true,
+      },
+    },
+  },
 });
