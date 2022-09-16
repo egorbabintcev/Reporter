42623ddd2cc35789cf91c737557909a68406c121
Егор Бабинцев <babintsev@trueconf.ru>
2022-09-16 22:31:31 +0300

    Добавлена библиотека с директивой маски
    - Подключена библиотека maska
    - Директива mask зарегистрирована в инициализационном файле

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import '@/styles/index.scss';
 
 import { createApp } from 'vue';
 
+import { maska } from 'maska';
+
 import store from '@/core/store';
 import router from '@/router';
 
 import App from './App.vue';
 
 createApp(App)
+  .directive('mask', maska)
   .use(store)
   .use(router)
   .mount('#app');
