97c61170dc8493860a80ed6d463c0968e8551dd0
Егор Бабинцев <babintsev@trueconf.ru>
2022-10-12 21:14:02 +0300

    Исправлена работа роутера
    - Исправлен функционал редиректа со всех незадекларированных роутов

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
       component: ReportsScreen,
     },
     {
-      path: '/(.*)',
+      path: '/:redirectAll(.*)',
       redirect: {
         name: 'reports',
       },
