c59fdbdbb51561997a8c00211171837e9bed78d5
Егор Бабинцев <babintsev@trueconf.ru>
2022-10-13 21:50:34 +0300

    Добавлены компоненты шапки карточки и карточки статистики

---
+++ src/screens/Reports/ReportsHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col gap-1.5">
+    <h1 class="text-xl font-bold text-gray-800">
+      Добро пожаловать, Егор!
+    </h1>
+
+    <h2 class="text-lg text-gray-600">
+      Здесь ты можешь просматривать свои отчеты, статистику и прочие плюшки 😊
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'ReportsHeader',
+  });
+</script>
+
+<style lang="scss">
+
+</style>
