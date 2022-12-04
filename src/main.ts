991d3b27287f97d8bc4cf7e6787c1d923284bfb7
Егор Бабинцев <babintsev@trueconf.ru>
2022-12-04 20:17:14 +0300

    Подключена локализация для ant design компонентов

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,17 @@ import Antd from 'ant-design-vue';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import 'dayjs/locale/ru';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(updateLocale);
+
+dayjs.locale('ru');
+dayjs.updateLocale('ru', {
+  weekStart: 0,
+});
 
 import store from '@/core/store';
 import router from '@/router';
